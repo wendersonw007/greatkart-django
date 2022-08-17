a16dc6525802bd7a427411195aad490154a77660
Wenderson <wenderson17@gmail.com>
2022-08-17 08:46:38 -0300

    primeiro commit

---
+++ store/models.py
@@ -0,0 +1,23 @@
+from distutils.command.upload import upload
+from itertools import product
+from operator import mod
+from unicodedata import category
+from django.db import models
+from category.models import Category
+
+# Create your models here.
+class Product(models.Model):
+    """Model referente aos produtos da aplicação Store"""
+    product_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(max_length=500, blank=True)
+    price = models.IntegerField()
+    images = models.ImageField(upload_to='photos/products')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_name
